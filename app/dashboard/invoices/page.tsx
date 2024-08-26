cae972405dcaebf93168c4550de5c665fabdafbb
sandjian <107805130+sandjian@users.noreply.github.com>
2024-08-26 16:27:14 -0300

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function invoices() {
+    return(
+        <div >
+          Dasboard/invoices
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+    <main className="flex h-screen flex-col p-6">
+      <div className="flex  shrink-0 items-end rounded-lg bg-blue-500 p-4 h-52">
+        <AcmeLogo />
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+      <div className="mt-4 flex grow flex-col gap-4">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 w-[22rem] md:w-[30rem]">
+          <p className={`text-2xl text-gray-800 md:text-3xl leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -24,9 +26,7 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-        </div>
+        
       </div>
     </main>
   );
